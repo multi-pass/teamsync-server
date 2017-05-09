5807b89a7ccf48d7e4e9d87a049d0dcbabfe2de8
Takashi Yoshi <takashi@yoshi.email>
2017-05-09 21:24:13 +0200

    Configure DB container

--- src/dependencies.php
+++ src/dependencies.php
@@ -3,6 +3,25 @@
 
 $container = $app->getContainer();
 
+use \RedBeanPHP\R as R;
+$container['db'] = function ($container) {
+	$dbconf = $container['parameters']['database'];
+
+	$db_type = ($dbconf['type'] ? $dbconf['type'] : 'mysql');
+	$db_host = ($dbconf['host'] ? $dbconf['host'] : '127.0.0.1');
+	$db_name = ($dbconf['dbname'] ? $dbconf['dbname'] : 'teamsync');
+
+	R::setup(($db_type.':host='.$db_host.'; dbname='.$db_name),
+			 $dbconf['user'],
+			 $dbconf['password']);
+
+	// Load custom model formatter
+	// RedBean_ModelHelper::setModelFormatter(new ModelFormatter);
+
+	// Leave fluid mode
+	R::freeze(TRUE);
+};
+
 $settings = $container->get('settings');
 $template_path = $settings['renderer']['template_path'];
 $container['renderer'] = new \Slim\Views\PhpRenderer($template_path);
