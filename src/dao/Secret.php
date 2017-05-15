f55716ddf842731436a6457f0c914d97d8dd19b5
Takashi Yoshi <takashi@yoshi.email>
2017-05-15 15:18:37 +0200

    Add functions to Secret DAO

--- src/dao/Secret.php
+++ src/dao/Secret.php
@@ -13,8 +13,40 @@ class Secret extends \RedBeanPHP\SimpleModel {
 	 * @param string $path
 	 * @return array of beans that represent a secret at the given path
 	 */
-	public static function findByPath($path) {
+	public static function findAllByPath($path) {
 		return R::findAll('secret', ' filepath = ? ', array($path));
+	}
+
+	/**
+	 * Get secret stored at a given path for user with $pgpid.
+	 *
+	 * @param string $path
+	 * @param string $pgpid
+	 * @return bean that represent a secret at the given path for user with $pgpid
+	 */
+	public static function findByPath($path, $pgpid) {
+		// TODO: Check for PGP id
+		return R::findOne('secret', ' filepath = ? ', array($path));
+	}
+
+	/**
+	 * Get all secrets stored for user with $pgpid.
+	 *
+	 * @param string $pgpid
+	 * @return array of beans that represents all secrets for user with $pgpid
+	 */
+	public static function findAll($pgpid) {
+		return R::findAll('secret');
+	}
+
+	/**
+	 * Get a list of all secrets stored for user with $pgpid.
+	 *
+	 * @param string $pgpid
+	 * @return array of string that represents all secrets for user with $pgpid
+	 */
+	public static function listAll($pgpid) {
+		return R::getCol('SELECT filepath FROM secret');
 	}
 
 }
