d4480279d4d88a937b69e14701e6bcbe4f7d21e5
Takashi Yoshi <takashi@yoshi.email>
2017-05-15 15:20:34 +0200

    Fix GetSecretsCommand

--- src/classes/GetSecretsCommand.php
+++ src/classes/GetSecretsCommand.php
@@ -2,7 +2,44 @@
 use \RedBeanPHP\R as R;
 class GetSecretsCommand extends Command {
 	public function run($model) {
-		$this->commandResult->data = R::getAll('SELECT * FROM secret');
+		$pgpid = TeamSyncSession::$current->publicKey;
+		$secrets = \TeamSync\DAO\Secret::listAll($pgpid);
+		sort($secrets);
+
+		$out = array(
+			'path' => '/',
+			'secrets' => array(),
+			'folders' => array()
+		);
+
+		foreach ($secrets as $s) {
+			$p =& $out;
+			if ("/" !== dirname($s)) {
+				$path = explode('/', dirname($s));
+				array_shift($path);
+
+				foreach ($path as $c) {
+					if (!isset($p['folders'][$c])) {
+						$p['folders'][$c] = array(
+							'path' => $c,
+							'secrets' => array(),
+							'folders' => array()
+						);
+					}
+					$p =& $p['folders'][$c];
+				}
+			}
+
+			$sec = \TeamSync\DAO\Secret::findByPath($s, $pgpid);
+			array_push($p['secrets'], array(
+				'name' => basename($sec->filepath),
+				'hash' => array(
+					'sha256' => $sec->hash
+				)
+			));
+		}
+
+		$this->commandResult->data = $out;
 		$this->commandResult->statusCode = 200;
 	}
 }
