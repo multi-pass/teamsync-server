957fdfdfdcc116b0a1f653c78b17ad65a6d6df09
Takashi Yoshi <takashi@yoshi.email>
2017-05-15 19:27:57 +0200

    Add methods to Recipient DAO

--- src/dao/Recipient.php
+++ src/dao/Recipient.php
@@ -1,7 +1,29 @@
 <?php
 
 namespace TeamSync\DAO;
+use \RedBeanPHP\R as R;
 
 class Recipient extends \RedBeanPHP\SimpleModel {
+
+	/**
+	 * Get recipient bean for a given PGP-id.
+	 *
+	 * @param string $pgpid
+	 * @return bean that represents a user with $pgpid
+	 */
+	public static function forKey($pgpid) {
+		$rec = NULL;
+		if (!($rec = R::findOne('recipient', ' `pgpid` = ? ', array($pgpid)))) {
+			$rec = R::dispense('recipient');
+			$rec->pgpid = $pgpid;
+		}
+
+		return $rec;
+	}
+
+
+	public function hasAccessToSecret(\RedBeanPHP\OODBBean $secret) {
+		array_push($secret->sharedRecipientList, $this->bean);
+	}
 
 }
