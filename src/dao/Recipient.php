1d9f94044667c8a85cf95d9ec9283b818af02846
Takashi Yoshi <takashi@yoshi.email>
2017-05-15 21:32:52 +0200

    Split factory methods for Recipients to fingerprint and pgpid

--- src/dao/Recipient.php
+++ src/dao/Recipient.php
@@ -6,14 +6,33 @@ use \RedBeanPHP\R as R;
 class Recipient extends \RedBeanPHP\SimpleModel {
 
 	/**
-	 * Get recipient bean for a given PGP-id.
+	 * Get recipient bean for a given PGP fingerprint.
+	 *
+	 * @param string $fingerprint
+	 * @return bean that represents a user with $fingerprint
+	 */
+	public static function forFingerprint($fingerprint) {
+		$rec = NULL;
+		if (!($rec = R::findOne('recipient', ' `fingerprint` = ? ', array($pgpid)))) {
+			$rec = R::dispense('recipient');
+			$rec->fingerprint = $fingerprint;
+		}
+
+		return $rec;
+	}
+
+	/**
+	 * Get recipient bean for a given PGP key id.
 	 *
 	 * @param string $pgpid
 	 * @return bean that represents a user with $pgpid
 	 */
 	public static function forKey($pgpid) {
-		$rec = NULL;
-		if (!($rec = R::findOne('recipient', ' `pgpid` = ? ', array($pgpid)))) {
+		$rec = R::findOne('recipient',
+						  ' `fingerprint` REGEXP :regexp OR `pgpid` REGEXP :regexp ',
+						  array('regexp' => ($pgpid.'$')));
+
+		if (is_null($rec)) {
 			$rec = R::dispense('recipient');
 			$rec->pgpid = $pgpid;
 		}
